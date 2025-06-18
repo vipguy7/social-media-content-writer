c36c4f2bca72ee890c10722b6d5334121a9681b0
vipguy7 <124065582+vipguy7@users.noreply.github.com>
2025-06-18 19:32:33 +0630

    save button

--- src/components/GeneratedContentOutput.tsx
+++ src/components/GeneratedContentOutput.tsx
@@ -81,6 +81,7 @@ const GeneratedContentOutput = ({ content, onCopy, onExportAll, onSave }: Genera
                     </Button>
                     <Button
                       onClick={handleSave}
+                      variant="outline"
                       disabled={selectedItems.length === 0 || isSaving}
                       size="sm"
                       className="btn-visible font-semibold flex-1 min-w-0"
